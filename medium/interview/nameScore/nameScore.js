417510f94d2bae7b00b3e0151d8451d6c4b161ba
n0252594 <rushil.shah@libertymutual.com>
2021-11-15 08:11:27 -0500

    nameScore master

---
+++ medium/interview/nameScore/nameScore.js
@@ -0,0 +1,25 @@
+const SCORES = {"A": 100, "B": 14, "C": 9, "D": 28, "E": 145, "F": 12, "G": 3,
+"H": 10, "I": 200, "J": 100, "K": 114, "L": 100, "M": 25,
+"N": 450, "O": 80, "P": 2, "Q": 12, "R": 400, "S": 113,
+"T": 405, "U": 11, "V": 10, "W": 10, "X": 3, "Y": 210, "Z": 23}
+
+const nameScore = (str) => {
+  const nameArray = str.toUpperCase().split('');
+  const scoreArray = nameArray.map(char => SCORES[char]);
+  const score = scoreArray.reduce((pV, currentValue) => pV + currentValue);
+
+  if(score <= 60) {
+    return "NOT TOO GOOD";
+  }
+  if(score <= 61 && score <= 300 ) {
+    return "PRETTY GOOD";
+  }
+  if(score <= 301 && score <= 599) {
+    return "VERY GOOD";
+  }
+  if(score >= 600) {
+    return "THE BEST";
+  }
+};
+
+module.exports = nameScore;
---
+++ medium/interview/nameScore/nameScore.test.js
@@ -0,0 +1,11 @@
+const nameScore = require("./nameScore");
+
+describe("nameScore", () => {
+  it("should function that takes a string of name and checks how much good is the given name. Add up the letters of your name to get the total score.", () => {
+    expect(nameScore("MUBASHIR")).toEqual("THE BEST");
+    expect(nameScore("YOU")).toEqual("VERY GOOD");
+    expect(nameScore("MATT")).toEqual("THE BEST");
+    expect(nameScore("PUBG")).toEqual("NOT TOO GOOD");
+
+  });
+});
